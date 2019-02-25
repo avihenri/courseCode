6f447d66437dcc49f62befa4c793743f5a83f5ac
avihenri <avril.m.henry@gmail.com>
2019-02-25 11:18:15 +0000

    added Iterators

---
+++ Courses/codecademy/JavaScript/#7_JS_HighOrderFunctions/#7_JS_Iterators.js
@@ -0,0 +1,8 @@
+// ITERATORS
+        // Introduction to Iterators
+            // iteration methods = built-in JS array methods
+                // .foreach() , .map() , .filter()
+
+            // The .forEach() Method
+            
+
