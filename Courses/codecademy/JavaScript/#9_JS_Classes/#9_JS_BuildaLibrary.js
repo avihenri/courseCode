24f8cc5c7ff1b83ed7f2b89ea0e8502da8c1db95
avihenri <avril.m.henry@gmail.com>
2019-03-20 14:14:23 +0000

    added project for classes

--- Courses/codecademy/JavaScript/#9_JS_Classes/#9_JS_BuildaLibrary.js
+++ Courses/codecademy/JavaScript/#9_JS_Classes/#9_JS_BuildaLibrary.js
@@ -25,30 +25,73 @@ If you’re looking for a challenge, try to create the four classes without usin
 */
 
 class Media {
-    constructor(title) {
-      this._title = title;
-      this._isCheckedOut = false;
-      this._ratings = [5, 3, 4, 1]; // test with 5, 3, 4, 1
-    }
-    get title() {
-      return this._title;
-    }
-    get isCheckedOut() {
-      return this._isCheckedOut;
-    }
-    get ratings() {
-      return this._ratings;
-    }
-    set isCheckedOut(status) {
-      this._isCheckedOut = status;
-    }
-    toggleCheckOutStatus() {
-      this._isCheckedOut = !this._isCheckedOut;
-    }
-    getAverageRating() {
-      let ratingTot = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
-      return ratingTot / this._ratings.length;
-    }
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = []; // test with 5, 3, 4, 1
   }
-  
-  console.log(Media.getAverageRating);
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+  set isCheckedOut(status) {
+    this._isCheckedOut = status;
+  }
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+    //console.log(this._isCheckedOut());
+  }
+  getAverageRating() {
+    let ratingTot = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+    return ratingTot / this._ratings.length;
+  }
+  addRating(newRating) {
+    this._ratings.push(newRating);
+  }
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }  
+}
+
+class Movie extends Media {
+  constructor(director, title, runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get director(){
+    return this._director;
+  }
+  get runTime() {
+    return this.runTime;
+  }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus(); 
+console.log(historyOfEverything.isCheckedOut); // true
+historyOfEverything.addRating(4, 5, 5);
+console.log(historyOfEverything.getAverageRating()); // 4
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut); // true
+speed.addRating(1, 1, 5);
+console.log(speed.getAverageRating()); // 1
+
