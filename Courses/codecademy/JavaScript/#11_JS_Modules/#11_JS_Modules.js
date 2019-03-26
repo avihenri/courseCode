660378b8fe364376dab8ef2ccaac052e68363be4
avihenri <avril.m.henry@gmail.com>
2019-03-26 16:35:03 +0000

    added more to modules

--- Courses/codecademy/JavaScript/#11_JS_Modules/#11_JS_Modules.js
+++ Courses/codecademy/JavaScript/#11_JS_Modules/#11_JS_Modules.js
@@ -126,3 +126,68 @@ and, importantly, prevent pollution of the global namespace and potential naming
           }
           
           displayStaffStatus();
+
+          // Export Named Exports
+            // The export keyword allows us to export objects upon declaration
+            export let availableAirplanes = [{
+              name: 'AeroJet',
+              fuelCapacity: 800,
+              availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+               maxSpeed: 1200,
+               minSpeed: 300,
+             }, 
+             {name: 'SkyJet',
+              fuelCapacity: 500,
+              availableStaff: ['pilots', 'flightAttendants'],
+              maxSpeed: 800,
+              minSpeed: 200,
+             }];
+             
+             export let flightRequirements = {
+               requiredStaff: 4,
+               requiredSpeedRange: 700,
+             };
+             
+             export function meetsStaffRequirements(availableStaff, requiredStaff) {
+               if (availableStaff.length >= requiredStaff) {
+                 return true;
+               } else {
+                 return false;
+               }
+             };
+             
+             export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+               let range = maxSpeed - minSpeed;
+               if (range > requiredSpeedRange) {
+                 return true;
+               } else {
+                 return false;
+               }
+             }
+
+            // Import Named Imports
+             // To import variables that are declared, we simply use the original syntax that describes the variable name. 
+             import {availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements} from './airplane';
+
+             function displayFuelCapacity() {
+               availableAirplanes.forEach(function(element) {
+                 console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+               });
+             }             
+             displayFuelCapacity();             
+             function displayStaffStatus() {
+               availableAirplanes.forEach(function(element) {
+                console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+               });
+             }             
+             displayStaffStatus();             
+             function displaySpeedRangeStatus() {
+               availableAirplanes.forEach(function(element){
+                 console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+               });  
+             }             
+             displaySpeedRangeStatus()
+
+            // Export as
+            // The as keyword allows us to give a variable name an alias
+             
