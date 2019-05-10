070ef07ff8d279f03267ec9ab789bd5882b16018
avihenri <avril.m.henry@gmail.com>
2019-05-10 14:23:56 +0100

    added functions

--- Courses/Learn-PHP.org/PHP-Tutorials.php
+++ Courses/Learn-PHP.org/PHP-Tutorials.php
@@ -520,15 +520,40 @@ while ($index < count($numbers)) {
 97 */
 
 
-
-
-
 // Functions
+    // two types
+        // 1. Library functions
+            // part of the php library - everyone can use
+        // 2. User functions
+            // Your own functions - only you cna use
+    
+        // define a function called `sum` that will
+        // receive a list of numbers as an argument.
+        function sum($numbers) {
+            // initialize the variable we will return
+            $sum = 0;
+            // sum up the numbers
+            foreach ($numbers as $number) {
+                $sum += $number;
+            }
+            // return the sum to the user
+            return $sum;
+        }
+        // Example usage of sum
+        echo sum([1,2,3,4,5,6,7,8,9,10]); // OUTPUT: 55
 
-
-
-
-
+    // EXERCISE:
+    <?php
+    // Write the function squared_sum here    
+    function squared_sum($numbers) {
+         $sum = 0;        
+        foreach ($numbers as $number) {
+            $sum += $number * $number;
+        }        
+        return $sum;
+    }    
+    echo squared_sum([56, 65, 26, 86, 66, 34, 78, 74, 67, 18, 34, 73, 45, 67, 75, 10, 60, 80, 74, 16, 86, 34, 12, 23, 42, 72, 36, 3, 73, 9, 92, 81, 94, 54, 97, 74, 45, 55, 70, 94, 96, 81, 86, 86, 84, 4, 32, 8, 96, 86, 87, 18, 84, 87, 59, 48, 32, 90, 17, 22, 82, 79, 66, 28, 17, 14, 80, 83, 66, 36, 21, 89, 68, 2, 51, 65, 20, 87, 48, 5, 1, 16, 60, 53, 84, 90, 16, 2, 37, 73, 57, 70, 57, 69, 68, 1, 24, 40, 72, 97]);    
+    ?> // OUTPUT: 382629
 
 
 // Objects
