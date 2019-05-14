498399e004986013b630459baddd5bc029584894
avihenri <avril.m.henry@gmail.com>
2019-05-14 11:23:29 +0100

    added 1st W3 Tutorial

---
+++ Courses/Learn-PHP/W3/PHP-W3-Tutorials.php
@@ -0,0 +1,9 @@
+<?php
+
+    // PHP opening and closing Tags syntax
+        // The default syntax starts with "<? php" and ends with ? >.
+        // short tags <? ? > (together)
+        // HTML Script tags
+        <script language="php">
+        echo "this is a HTML scrpt tag";
+        </script>
