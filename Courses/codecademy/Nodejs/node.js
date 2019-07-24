2719253e092c2173e891b2f0ec73e45da82cb9a2
avihenri <avril.m.henry@gmail.com>
2019-07-24 11:42:07 +0100

    added process object exercise

--- Courses/codecademy/Nodejs/node.js
+++ Courses/codecademy/Nodejs/node.js
@@ -32,3 +32,49 @@ console.log('Heya!'); // to check, follow the below
     // in bash go to directory of the js program (eg. app.js)
     // type - node app.js 
     // Heya! prints
+
+// Accessing the Process Object - Doc https://nodejs.org/api/process.html
+    // The process object is like the Task Manager for windows or Activity Monitor for Mac
+        // process.env = stores & controls info about the environment for which the process is running
+            // PWD property in process.env = string with directory in which the current process is located
+            // An app in development stage may perform differently that in production
+                // may need some extra login in development
+                // should add aproperty to process.env called NODE_ENV with a value of development or production
+                if (process.env.NODE_ENV === 'development'){
+                    console.log('Testing! Testing! Does everything work?');
+                  }
+        // process.memoryUsage() = returns CPU info for current process..
+            // { rss: 26247168,
+            // heapTotal: 5767168,
+            // heapUsed: 3573032,
+            // external: 8772 }
+                // Heap = means different things in different contexts
+                    // could refer to a specific data structure
+                    // or refer to a block of computer memory
+                    // process.memoryUsage.heapUsed will return a number of bytes of memory the process is using
+        //process.argv = holds array of cmd values provided when process was initiated 
+            // 1st element = absolute path to Node which ran the process
+            // 2nd element =path to file that's running
+            // cmd line arguements (each are saparated with spaces)
+                  // node myProgram.js testing several features
+                  console.log(process.argv[3]); // Prints 'several'
+    // EXERCISE:
+        let initialMemory = process.memoryUsage().heapUsed;
+        let word = process.argv[2];        
+        console.log(`Your word is ${word}`)        
+        // Create a new array
+        let wordArray = [];        
+        // Loop 1000 times, pushing into the array each time 
+        for (let i = 0; i < 1000; i++){
+        wordArray.push(`${word} count: ${i}`)        }
+        
+        console.log(`Starting memory usage: ${initialMemory}. 
+        \nCurrent memory usage: ${process.memoryUsage().heapUsed}. 
+        \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
+        // In bash
+            // $ node app.js shattered >
+            //  Your word is shattered
+            // Starting memory usage: 3144256.
+            // Current memory usage: 4052440.
+            // After using the loop to add elements to the array, the process is using 911792 more bytes of memory.
+    
