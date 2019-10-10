269ba566d25f96edee6631d59acf905910bf08e7
avihenri <avril.m.henry@gmail.com>
2019-10-10 10:29:36 +0100

    half completed

--- Courses/codecademy/PHP/7.3.LogicOps_CompoundCondi.php
+++ Courses/codecademy/PHP/7.3.LogicOps_CompoundCondi.php
@@ -26,3 +26,76 @@
           echo "I'm pushing the button.";
       }
    }
+
+   // FIRST EXERCISE
+    function both($first, $second) {
+      if ($first) {
+        if ($second) {
+          return 'both';
+        } else {
+          return 'not both';
+        }
+      } else {
+        return 'not both';
+      }
+    }
+  echo both(true, false); // NOT BOTH
+  echo both(true, true); // BOTH
+
+  // The || Operator
+    // takes 2 boolean values or expresion as its operands & returns a single boolean value
+    // returns true if either the left or right operands are true
+    TRUE || TRUE;   // Evaluates to: TRUE
+    FALSE || TRUE;  // Evaluates to: TRUE    
+    TRUE || FALSE;  // Evaluates to: TRUE    
+    FALSE || FALSE; // Evaluates to: FALSE
+
+    // password reset example
+    $is_admin = FALSE;
+    $is_user = TRUE;
+    if ($is_admin || $is_user){
+      echo "You can change the password";
+    } // true so prints the above out
+
+    // WORKING EXAMPLE: HUNGRY & DESSERT
+    function willWeEat($meal, $hungry) {
+      if ($hungry || $meal === "dessert" ) {
+        return "Yum. Thanks!\n";
+      } else {
+        return "No thanks. We're not hungry.\n";
+      }
+    }
+    echo willWeEat("lunch",false); // No thanks. We're not hungry.
+    echo willWeEat("dessert", false); // Yum. Thanks!
+    echo willWeEat("lunch", true); // Yum. Thanks!
+    echo willWeEat("dessert", true); // Yum. Thanks!
+
+    // The && Operator
+      // returns true if both operands are true
+      // returns false if either or both operands are false
+      // higher operator precedence than the || operator - so will be evaluated first in complex statements
+      TRUE && TRUE;    // Evaluates to: TRUE
+      FALSE && TRUE;   // Evaluates to: FALSE
+      TRUE && FALSE;   // Evaluates to: FALSE
+      FALSE && FALSE;  // Evaluates to: FALSE
+      TRUE || TRUE && FALSE; // Evaluates to: TRUE
+      (TRUE || TRUE) && FALSE; // Evaluates to: FALSE parentheses make the || go first
+
+      // ATM example
+      $correct_pin = TRUE;
+      $sufficient_funds = TRUE;
+      if ($correct_pin && $sufficient_funds) {
+        echo "You can make the withdrawal.";
+      }
+
+      // Happy and you know it
+      function clapYourHands($happiness, $knowIt) {
+        if ($happiness && $knowIt) {
+          return "CLAP!";
+        } else {
+          return ":(";
+        }
+      }
+      echo clapYourHands(true, true); // CLAP!
+      echo clapYourHands(false, true); // :(
+      echo clapYourHands(true, false); // :(
