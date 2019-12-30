e538caf79c9acb264f7d9cbdadc5e26f6cd2014f
avihenri <avril.m.henry@gmail.com>
2019-12-30 16:26:38 +0000

    custom form validation

--- Courses/codecademy/PHP/9.0.FormValidation.php
+++ Courses/codecademy/PHP/9.0.FormValidation.php
@@ -317,3 +317,71 @@ ANSWER - \d?\s?\(?\d{3}\)?\s?\-?\.?\d{3}\-?\s?\.?\d{3,4}
         <input type="submit" value="Submit">
     </form>
         <p><?= $message;?></p>
+
+<!-- CUSTOM VALIDATION -->
+      <!-- PRE_MATCH(string $pattern , string $subject [, array &$matches [, int $flags = 0 [, int $offset = 0 ]]] ) : int -->
+      <?php
+      $pattern = '/^[(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4}$/';
+      preg_match($pattern, "(999)-555-2222"); // Returns: 1      
+      preg_match($pattern, "555-2222"); // Returns: 0
+    //   preg_match could take up a lot of computing power if the string is very long do we can use strlen() to check the length
+    $name = "Aisle Nevertell";
+    $length = strlen($name);
+    if ($length > 2 && $length < 100){
+      echo "That seems like a reasonable name to me...";
+    } ?>
+    <?php
+$feedback = "";
+$success_message = "Thank you for your donation!";
+$error_message = "* There was an error with your card. Please try again.";
+
+$card_type = "";
+$card_num = "";
+$donation_amount = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $card_type = $_POST["credit"];
+    $card_num = $_POST["card-num"];
+    $donation_amount = $_POST["amount"];
+
+// Write your code here:
+  
+  
+  if (strlen($card_num) < 100) {
+    if ($card_type == "mastercard") {
+      $pattern = "/5[1-5][0-9]{14}/";
+      if (preg_match($pattern, $card_num)) {
+        $feedback = $success_message;
+      } else {
+        $feedback = $error_message;
+      }
+    } else if ($card_type == "visa") {
+      $pattern = "/4[0-9]{12}([0-9]{3})?([0-9]{3})?/";
+      if (preg_match($pattern, $card_num)) {
+        $feedback = $success_message;
+      } else {
+        $feedback = $error_message;
+      }
+  }   
+} else {
+    $feedback = $error_message;
+  }
+}
+?>
+<form action="" method="POST">
+  <h1>Make a donation</h1>
+    <label for="amount">Donation amount?</label>
+      <input type="number" name="amount" value="<?= $donation_amount;?>">
+      <br><br>
+    <label for="credit">Credit card type?</label>
+      <select name="credit" value="<?= $card_type;?>">
+        <option value="mastercard">Mastercard</option>
+        <option value="visa">Visa</option>
+      </select>
+      <br><br>
+      <label for="card-num">Card number?</label>
+      <input type="number" name="card-num" value="<?= $card_num;?>">
+      <br><br>   
+      <input type="submit" value="Submit">
+</form>
+<span class="feedback"><?= $feedback;?></span>
