c19b6ccfa1ee14ab0699f6faf7f2fa645a23efd1
avihenri <avril.m.henry@gmail.com>
2019-12-31 08:29:51 +0000

    finished form validation

--- Courses/codecademy/PHP/9.0.FormValidation.php
+++ Courses/codecademy/PHP/9.0.FormValidation.php
@@ -385,3 +385,152 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <input type="submit" value="Submit">
 </form>
 <span class="feedback"><?= $feedback;?></span>
+
+<!-- Validating Against Back-end Data -->
+  <?php 
+  // In the example below, we model the database of users with the associative array $users, 
+  // which contains keys in the format "username" => "password".
+    $users = ["coolBro123" => "password123!", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];
+    function isUsernameAvailable ($username){
+      global $users;
+      if (isset($users[$username])){
+        echo "That username is already taken.";
+      } else {
+        echo "${username} is available.";
+      }
+    }
+    isUsernameAvailable("coolBro123");
+    // Prints: That username is already taken. 
+    isUsernameAvailable("aisleOfPHP");
+    // Prints: aisleOfPHP is available.
+
+    // We need to check two things.
+    // That the username is key on the array:
+    isset($users[$username])
+    // That the password is the same as that key’s value:
+    $users[$username] === $password
+    // EXAMPLE
+    $users = ["coolBro123" => "password123!", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];   // WOULD IN DATABASE
+    $feedback = "";
+    $message = "You're logged in!";
+    $validation_error = "* Incorrect username or password.";
+    $username = "";
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+      $username = $_POST["username"];
+      $password = $_POST["password"];
+      if (isset($users[$username]) && $users[$username] === $password) {
+        $feedback = $message;
+      } else {
+        $feedback = $validation_error;
+      }
+    }
+    ?>  
+    <h3>Welcome back!</h3>
+    <form method="post" action="">
+    Username:<input type="text" name="username" value="<?php echo $username;?>">
+    <br>
+    Password:<input type="text" name="password" value="">
+    <br>
+    <input type="submit" value="Log in">
+    </form>
+    <span class="feedback"><?= $feedback;?></span>
+    
+<!-- Sanitizing for Back-end Storage -->
+    <?php
+    // TWO VERY IMPORTING THINGS TO DO TO HELP SECURITY -
+      // SANITIZE DATA - preg_replace()
+          $one = "codeacademy";
+          $two = "CodeAcademy";
+          $three = "code academy";
+          $four = "Code Academy";          
+          $pattern = "/[cC]ode\s*[aA]cademy/";
+          $codecademy = "Codecademy";          
+          echo preg_replace($pattern, $codecademy, $one);
+          // Prints: Codecademy          
+          echo preg_replace($pattern, $codecademy, $two);
+          // Prints: Codecademy          
+          echo preg_replace($pattern, $codecademy, $three);
+          // Prints: Codecademy          
+          echo preg_replace($pattern, $codecademy, $four);
+          // Prints: Codecademy
+      // FORMAT DATA
+
+        $contacts = ["Susan" => "5551236666", "Alex" => "7779991717", "Lily" => "8181117777"];  
+        $message = "";
+        $validation_error = "* Please enter a 10-digit North American phone number.";
+        $name = "";
+        $number = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $name = $_POST["name"];
+          $number  = $_POST["number"];
+          // Write your code here:
+          if (strlen($number) < 30) {
+            $pattern = "/[^0-9]/";
+            $formatted_number = preg_replace($pattern, "", $number);
+            if (strlen($formatted_number) === 10) {
+              $contacts[$name] = $formatted_number;
+              $message = "Thanks ${name}, we'll be in touch.";
+            }
+            else {
+              $message = $validation_error;
+            }
+          } else {
+            $message = $validation_error;     
+          }
+        };
+        ?>
+        <html>
+          <body>
+          <h3>Contact Form:</h3>
+            <form method="post" action="">
+              Name:
+              <br>
+              <input type="text" name="name" value="<?= $name;?>">
+              <br><br>
+              Phone Number:
+              <br>
+              <input type="text" name="number" value="<?= $number;?>">
+              <br><br> 
+              <input type="submit" value="Submit">
+            </form>
+            <div id="form-output">
+              <p id="response"><?= $message?></p>
+            </div>
+          </body>
+        </html>
+
+<!-- Rerouting -->
+    <?php
+    //  header()
+        // use "Location: url"
+        // after invoking the header() function we need to use the language construct.. exit ..to terminate the current script
+        // header function needs to be run before anything output by the script so before our file outputs HTML
+        
+        if (/* Is the submission data validated? */) {
+          header("Location: https://www.best-puppy-pix.com/");
+          exit;
+        }
+    
+?>
+
+<!-- Review
+Great work! We covered a lot in this lesson. Let’s review:
+
+- Performing back-end form validations on the data submitted is an essential step to protect our website and its users.
+- Using the POST method attribute in an HTML form gives our PHP script access to data submitted within the superglobal associative array: $_POST.
+- We modify our HTML and PHP so that when input is deemed invalid, meaningful feedback is shown to the user.
+- If we plan on displaying user input, we need to first sanitize it. We can use methods like trim() and htmlspecialchars() for basic sanitization.
+- We can use filter_var() with a filter to sanitize common input types.
+- We can also use filter_var() with a filter to perform validations on common input types.
+- We’ll often want to perform custom validations.
+- The preg_match() function compares checks if a given string matches a regular expression.
+- Since regular expression comparisons can consume a lot of computing power, we’ll want to check the length of inputs before performing regular expression checks.
+- It’s common to perform validations by comparing user input to back-end data
+- Before storing user input in our back-end, we’ll sanitize it for both safety and consistent formatting
+- If a user’s form submission has been accepted, we can reroute them to a different page.
+- Data validation and sanitization is an extremely important part of web development. In this lesson, we’ve covered some of the basic theory and techniques. 
+  When developing for production, you’ll need to further research and understands the needs of the specific sites or applications as well as the tools 
+  available with the specific databases or frameworks in use.
+
+As your validations get more and more complex, you should also practice modularity and separate your validation logic from your display logic. -->
