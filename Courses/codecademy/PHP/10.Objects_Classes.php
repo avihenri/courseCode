4e724ab4a7b3fe606b7552122d6c575c9aa64644
avihenri <avril.m.henry@gmail.com>
2020-01-09 11:34:44 +0000

    Methods & instantiating objects

--- Courses/codecademy/PHP/10.Objects_Classes.php
+++ Courses/codecademy/PHP/10.Objects_Classes.php
@@ -23,4 +23,34 @@
         // Creating objects = instantiation
         // Objects are instantiated using key work = new
         $very_good_dog = new Pet(); // - new object
+        // to interact with the objects properties we use..   -?
+        $very_good_dog->name = "Lassie";
+        echo $very_good_dog->name; # Prints "Lassie"
         
+    // Methods
+        // define class methods - functions each object will contain
+        class Pet {
+            public $first, $last;
+            function getFullName() {
+              return $this->first . " " . $this->last; // $this refers to current object
+            }
+        }
+        // to access methods we use ->
+        $my_object->classMethod();
+
+        $very_good_groundhog = new Pet();
+        $very_good_groundhog->first = "Punxsutawney";
+        $very_good_groundhog->last = "Phil";
+        echo $very_good_groundhog->getFullName(); # Prints "Punxsutawney Phil"
+        //WORKING EXAMPLE
+        class Beverage {
+            public $temperature, $color, $opacity;
+            function getInfo() {
+              return "This beverage is $this->temperature and $this->color.";
+            }
+          }       
+          $soda = new Beverage();
+          $soda->color = "black";
+          $soda->temperature = "cold";
+          echo $soda->getInfo(); // This beverage is cold and black.
+          
