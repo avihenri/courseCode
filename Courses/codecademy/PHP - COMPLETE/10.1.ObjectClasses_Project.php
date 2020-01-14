e5250302051420edf3566e4e85307b203c1abe80
avihenri <avril.m.henry@gmail.com>
2020-01-14 09:33:05 +0000

    Edits to pages and folder

---
+++ Courses/codecademy/PHP - COMPLETE/10.1.ObjectClasses_Project.php
@@ -0,0 +1,72 @@
+<?
+/*
+LEARN PHP
+Going to Bed
+This project provides practice for:
+
+Creating objects from classes
+Writing and using methods
+Using static methods
+*/
+
+/* 
+In this project we will be creating some pajamas for barnyard animals. The farmer has a special way that she likes names formatted. 
+To ensure this, we are going to build our own string function similar to PHP’s built in strtolower or strtoupper.
+
+We’re going to put this function inside of a helper class StringUtilities (making it a method). We can then add other string utilities
+ or reuse this class in another project for the same farmer.
+*/
+<?php
+
+class StringUtilities {
+  public static function secondCase($str) {
+    $str = strtolower($str);
+    if (strlen($str) >=2){
+    $str[1] = strtoupper($str[1]);
+    }
+    return $str;
+  }  
+}
+
+class Pajamas {
+  private $owner, $fit, $color;
+  function __construct(
+    $owner = "TBC",
+    $fit = "good",
+    $color = "black"
+    ) {
+    $this->owner = StringUtilities::secondCase($owner);
+    $this->fit = $fit;
+    $this->color = $color;
+  }
+  public function describe() {
+    return "Owner: $this->owner,\n Fit: $this->fit,\n Colour: $this->color\n";
+  }
+  public function setFit($new_fit) {
+    $this->fit = $new_fit;
+  }
+}
+
+class ButtonablePajamas extends Pajamas {
+  private $button_state  = "unbottoned";
+  public function describe() {
+    return parent::describe() . " Button Status: $this->button_state"; 
+  }
+  public function toggleButtons() {
+    if ($this->button_state == "unbottoned") {
+      $this->button_state = "buttoned";
+    } elseif ($button_state == "bottoned") {
+      $this->button_state = "unbuttoned";
+    }
+  }
+}
+
+$chicken_PJs = new Pajamas('CHICKEN', 'great', 'tropical teal');
+echo $chicken_PJs->describe();
+$chicken_PJs->setFit('bit tight');
+echo $chicken_PJs->describe();
+
+$moose_PJs = new ButtonablePajamas("moose");
+echo $moose_PJs->describe();
+echo $moose_PJs->toggleButtons();
+echo $moose_PJs->describe();
