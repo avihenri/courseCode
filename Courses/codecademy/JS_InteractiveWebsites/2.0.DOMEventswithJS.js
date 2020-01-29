395d47275e641cb28a4868ee7b4d56ac08bc7ee4
avihenri <avril.m.henry@gmail.com>
2020-01-29 10:14:41 +0000

    Started DOM Events, no time at work

---
+++ Courses/codecademy/JS_InteractiveWebsites/2.0.DOMEventswithJS.js
@@ -0,0 +1,6 @@
+// DOM EVENTS WITH JAVASCRIPT
+    // What is an Event?
+        // Mouse click, webpages files loading in the browser, swiping right on image
+    
+    // Event Handler Registration
+        // 
