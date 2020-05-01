dd32acdeba13745afc683a2277c2ae128c190d0c
AviHenri <avril.m.henry@gmail.com>
2020-05-01 11:17:04 +0100

    Learn Vue complete - notes

--- Courses/codecademy/Vue/Intro-to-vue/Notes.js
+++ Courses/codecademy/Vue/Intro-to-vue/Notes.js
@@ -27,7 +27,7 @@ const app = new Vue({ // options object
     }
 });
 // Using data key-value pairs
-const app = new Vue({
+const app2 = new Vue({
   el: '#app',
   data: {
     language: 'Spanish',
@@ -41,7 +41,7 @@ const app = new Vue({
 
 // Computed object
 // - this can be used to calculate different values from the data key-value pairs we already have depending on conditions
-const app = new Vue({
+const app3 = new Vue({
   el: '#app',
   data: {
     hoursStudied: 274
@@ -63,7 +63,7 @@ const app = new Vue({
 </div>
 
 // Getter and setter in computed
-const app = new Vue({
+const app4 = new Vue({
     el: '#app',
     data: {
       hoursStudied: 274
@@ -117,7 +117,7 @@ const app = new Vue({
 //   The methods property is where Vue apps store their instance methods. These methods can be used in many situations, 
 // such as helper functions used in other methods or event handlers (functions that are called in response to specific user interactions).
 
-  const app = new Vue({
+  const app5 = new Vue({
     el: "#app",
     data: {
       hoursStudied: 300
@@ -138,3 +138,120 @@ const app = new Vue({
         specifying get and set functions — the setter will update other dynamic values when the computed value changes
     watch - used for performing functionality when a specified dynamic value changes
     methods - used for storing instance methods to be used throughout the app */
+
+  
+  // LESSON THREE - VUEW FORMS
+    // v-model - binds form fields with vue data or computed v-model="dataName"
+        // you can bind any type of input, select, textarea
+    // Radio button bindings
+        // Each radio button in a group will need their own v-model with the same value <input type="radio" id="goodReview" value="good" v-model="experienceReview" /> 
+    // Array checkbox bindings
+        // as multiple can be selected the data item needs to be an array data: { requestedFeatures: [] }  
+        // Each have the same v-model value
+    // Boolean checkbox bindings
+        // single checkbox with true or false - it will be set to false in the data and if selected will be changed to true
+    // Form event handlers
+        // v-on directive to add events - v-on:click="function", v-on:reset="function" the functions being in methods
+        // shorthand for v-on is @ - @click or @reset
+        // these need to be added to buttons and the form opening tag
+    // Form event Modifiers - https://vuejs.org/v2/api/#v-on
+        // such as Event.preventDefault on submit - v-on:submit.prevent="submitForm" (this is on the form element) <form v-on:reset="resetFields" v-on:submit.prevent="submitForm">
+        // stop modifier, it would call Event.stopPropagation()
+    // Input Modifiers - https://vuejs.org/v2/api/#v-model
+        // Modifiers for v-model = .number converts value to a number, .trim removes whitespaces from beginning and end, .lazy only updates values when change events are triggered
+    // Form validation
+        // disabled property on a button - v-bind:disabled="!formIsValid" - 
+            // we bind the disabled property to the submit button and call the computed function formIsValid
+            // the formIsValid function will check the values and if valid then the disabled button will disappear and the button will be enabled
+            // if not valid the disabled will be there and the button will not be enabled
+
+  /* Review
+      Congratulations, you now know how to bind HTML forms to Vue data and listen to events in Vue! Let’s review the major takeaways from this lesson:
+
+      Form fields can be bound to Vue data using the v-model directive — how v-model is used depends on the type of field it is being added to
+      Form event handlers can be added using v-on:submit and v-on:reset
+      Modifiers can be used to add functionality to directives — most importantly preventing page reload on form submission using v-on:submit.prevent and cleaning up form field values using .number and .trim
+      Form validation can be implemented by setting the value of the disabled attribute on a <button> to the value of a computed property using v-bind */
+
+
+  // Lesson Four - Styling elements with vue
+    // Inline Styles
+    <h2 v-bind:style="{ color: breakingNewsColor, 'font-size': breakingNewsFontSize }">Breaking News</h2>
+    const app6 = new Vue({ 
+      data: { 
+        breakingNewsColor: 'red',
+        breakingNewsFontSize: '32px'
+      }
+    });
+        // if a css property has a hyphen then we need to put it quotes - 'font-size'
+        // this used the data but computed properites can be used with v-bind:style
+    // Computed Style Objects
+        // We can refacter what we just did to have all the styles in a property
+        <h2 v-bind:style="breakingNewsStyles">Breaking News</h2>
+        const app7 = new Vue({ 
+          data: { 
+            breakingNewsStyles: { 
+              color: 'red',
+              'font-size': '32px'
+            }
+          }
+        });
+        // using computed properties we can style depending on its condition
+        function() {
+          if (this.formIsValid) {
+            return {
+              'background-color': '#4c7ef3',
+              cursor: 'pointer'
+            }
+          } else {
+            return {
+              'background-color': 'gray',
+              cursor: 'default'
+            }
+          }
+        }
+  // Multiple Style Objects
+      // v-bin:style can take an array of properties - v-bind:style="[newsHeaderStyles, breakingNewsStyles]"
+        // the style object added later in the array gets priority 
+
+  // CSS Classes
+      <span v-bind:class="{ unread: hasNotifications }">Notifications</span>
+      // .unread {
+      //   background-color: blue;
+      // }
+      const app = new Vue({
+        data: { notifications: [ ... ] },
+        computed: {
+          hasNotifications: function() {
+            return notifications.length > 0;
+          }
+        }
+      }
+      // Here we use v-bind:class to add class unread to notifications span if the computed hasNotifications is true
+      // v-bind:class takes an object as its value
+        // keys are the class names & values are the vue app properties that return truthy or falsy
+       
+    // CLass Arrays
+      // v-bind:class can take an array as its value
+      // YOu can add conditional classes like previous step but you can also have just classes in the array
+      <span v-bind:class="[{ unread: hasNotifications }, menuItemClass]">Notifications</span>
+      const app = new Vue({
+        data: { 
+          notifications: [ ... ],
+          menuItemClass: 'menu-item'
+        },
+        computed: {
+          hasNotifications: function() {
+            return notifications.length > 0;
+          }
+        }
+      }
+      // .menu-item {
+      //   font-size: 12px;
+      // }
+
+      // .unread {
+      //   background-color: blue;
+      // }
+      // if its just properties you don't need curly brackets - v-bind:class="[requiredFieldClass, emailClasses]"
+    
