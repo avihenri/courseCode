461b12c0453fefba64328d7ed8392ec4c444108a
AviHenri <avril.m.henry@gmail.com>
2020-05-04 20:50:17 +0100

    Intro to node & Advanced JS Notes

--- Courses/codecademy/Web_Dev_Path/Nodejs/node.js
+++ Courses/codecademy/Web_Dev_Path/Nodejs/node.js
@@ -183,4 +183,157 @@ console.log('Heya!'); // to check, follow the below
             // These APIs trigger the subscription to and emitting of events to signal the completion of the operation
             // once completed the callback function is added to the queue or line, or list or synchronous tasks 
             // so if sync tasks never end the event-queue would never run
-            // 
+            // NEED MORE HELP ON THIS!!
+
+    // User Input/Output
+        // console.log() = thin-wrapper on nodes stdout.write() which writes to the screen
+        // stdin.on() - can be used on the process method to get user input
+        process.stdin.on('data', (userInput) => {
+            let input = userInput.toString()
+            console.log(input)
+          });
+
+    // ERRORS   
+        // Node has all standard errors
+          // EvalError, SyntaxError, RangeError, ReferenceError, TypeError & URIError
+        // we can use try..catch for error handling
+        // many node api's use error first arguments
+          // if there was an async error it will be passed to the first argument but if not the 1st arg will be undefined
+          const errorFirstCallback = (err, data)  => {
+            if (err) {
+              console.log(`There WAS an error: ${err}`);
+            } else {
+               // err was falsy
+                console.log(`There was NO error. Event data: ${data}`);
+            }
+          }
+          // invoke api method - api.errorProneAsyncApi('problematic input', errorFirstCallback);
+
+    // Filesystem
+        // Its important that javascript scripts have limited access to filesystem
+        // this is called sandboxing
+          // it keeps users secure from malicious behaviour 
+        // node has less restrictions and can use fs core module to interact with the filsystem
+          // each method has a sync or async option
+          // .readFile() - reads data from a specific file
+        const fs = require('fs');
+
+        let readDataCallback = (err, data) => {
+        if (err) {
+            console.log(`Something went wrong: ${err}`);
+        } else {
+            console.log(`Provided file contained: ${data}`);
+        }
+        };
+
+        fs.readFile('./file.txt', 'utf-8', readDataCallback);
+        
+    // Readable streams
+        // normally we don't want to read whole files at once so we stream parts
+        // can do line-by-line - .createInterface() returns an EventEmitter set up to emit 'line' events:
+
+        // We require in the readline and fs core modules.
+        const readline = require('readline');
+        const fs = require('fs');
+
+        // We assign to myInterface the returned value from invoking readline.createInterface() with an object containing our designated input.
+        const myInterface = readline.createInterface({
+            // We set our input to fs.createReadStream('text.txt') which will create a stream from the text.txt file.
+            input: fs.createReadStream('text.txt')
+        });
+
+        // Next we assign a listener callback to execute when line events are emitted. A 'line' event will be emitted after each line from the file is read.
+        myInterface.on('line', (fileLine) => {
+            // Our listener callback will log to the console 'The line read: [fileLine]', where [fileLine] is the line just read.
+            console.log(`The line read: ${fileLine}`);
+        });
+
+        // writable streams
+            // we can write line-by-line createWriteStream
+        const fs = require('fs')
+        const fileStream = fs.createWriteStream('output.txt');
+
+        fileStream.write('This is the first line!'); 
+        fileStream.write('This is the second line!');
+        fileStream.end(); // writable streams don't stop automatically so we need to end
+
+
+        const myInterface = readline.createInterface({
+            input: fs.createReadStream('shoppingList.txt')
+          });
+          
+          const fileStream = fs.createWriteStream('shoppingResults.txt');
+          
+          let transformData = (line) => {
+            fileStream.write(`They were out of: ${line}\n`);
+          }
+          
+          myInterface.on('line', transformData)
+
+    // Create an HTTP Server
+          // need of node is to create web servers , computer processes that listen that listen for requests from clients & return responses
+          // http.createServer() - returns an instance of the http.server
+          // http.server has .listen() - to listen for requests
+          // when we run http.createServer() we pass in a custom callback function (requestListener)
+          // this will be triggered once the server is listening and receives requests
+        //   requestListener
+        //     - expects 2 arguments - request object & a response object
+        //     - each time a request is made - requestListener callback function is invoked - passing in the request & response objects of the incoming request
+        //     - request & response objects come with a number of properties & methods of their own
+        //     - requestListener is responsible for setting the response header & body
+        //     - it must signal that the interaction is complete by calling the response.end()
+
+        // We required in the http core module.
+        const http = require('http');
+        // We created a server variable assigned to the return value of the http.createServer() method.
+        let requestListener = (request, response) => {
+        // Within the requestListener callback, we make changes to the response object, response, 
+        // so that it can send the appropriate information to the client sending the request. 
+        // The status code 200 means that no errors were encountered. The header communicates that the file 
+        // type is text, rather than something like audio or compressed data.
+        response.writeHead(200, {'Content-Type': 'text/plain' });
+        response.write('Hello World!\n');
+        response.end();
+        };
+        // We invoked http.createServer() with our requestListener callback. 
+        // This is similar to running the .on() of an EventEmitter: the requestListener will execute 
+        // whenever an HTTP request is sent to the server on the correct port.
+        const server = http.createServer(requestListener);
+        // // The last line starts the server with the port 3000. Every server on a given machine specifies a unique port so that traffic can be correctly routed.
+        server.listen(3000);
+
+        /*
+        Review
+            Awesome work! You’ve learned a lot about Node.js including:
+
+            - Node.js is a JavaScript runtime, an environment that allows us to execute our JavaScript code by converting it into something a computer 
+            can understand.
+            - REPLs are processes that read, evaluate, print, and repeat (loop), and Node.js comes with its own REPL we can access in our terminal with 
+            the node command.
+            - We run JavaScript programs with Node in the terminal by typing node followed by the file name (if we’re in the same directory) or the 
+            absolute path of the file.
+            - Code can be organized into separate files, modules, and combined through requiring them where needed using the require() function.
+            - In addition to core modules, modules included within the environment to efficiently perform common tasks, we can also create our 
+            own modules using module.exports and the require() function.
+            - We can access NPM, a registry of hundreds of thousands of packages of re-usable code from other developers, directly through our terminal.
+            - Node has an event-driven architecture.
+            - We can make our own instances of the EventEmitter class and we can subscribe to listen for named events with the .on() method and emit 
+            events with the .emit() method.
+            - Node uses an event loop which enables asynchronous actions to be handled in a non-blocking way by adding callback functions to a queue 
+            of tasks to be executed when the callstack is empty.
+            - In order to handle errors during asynchronous operations, provided callback functions are expected to have an error as their first parameter.
+            - Node allows for both output, data/feedback to a user provided by a computer, and input data/feedback to the computer provided by the user.
+            - - The Node fs core module is an API for interacting with the file system.
+            - Streams allow us to read or write data piece by piece instead of all at once.
+            - The Node http core module allows for easy creation of web servers, computer processes that listen for requests from clients and return responses.
+
+            */
+        
+        
+        
+        
+        
+        
+        
+        
+        
