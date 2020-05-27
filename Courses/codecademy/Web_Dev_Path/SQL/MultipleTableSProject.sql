3e3e0f5fd5cd67123b99313bc42ad3d27b527d53
avihenri <avril.m.henry@gmail.com>
2020-05-27 08:22:41 +0100

    SQL project

--- Courses/codecademy/Web_Dev_Path/SQL/MultipleTableSProject.sql
+++ Courses/codecademy/Web_Dev_Path/SQL/MultipleTableSProject.sql
@@ -8,3 +8,46 @@
 -- trips - trips information
 -- riders - users data
 -- cars - autonomous cars
+
+SELECT * FROM trips;
+
+SELECT * FROM riders;
+
+SELECT * FROM cars;
+
+SELECT riders.first,
+   riders.last,
+   cars.model
+FROM riders, cars;
+
+SELECT *
+from trips
+LEFT JOIN riders
+  ON trips.rider_id = riders.id;
+
+SELECT *
+FROM trips
+INNER JOIN cars
+  ON trips.car_id = cars.id;
+
+ SELECT * 
+ FROM riders
+ UNION
+ SELECT * 
+ FROM riders2;
+
+SELECT round(avg(cost), 2)
+FROM trips;
+
+SELECT * 
+FROM riders
+WHERE total_trips < 500;
+
+SELECT COUNT(*)
+FROM cars
+WHERE status = 'active';
+
+SELECT * 
+FROM cars
+ORDER BY trips_completed DESC
+limit 2;
