c902c2de004cf25bdc14123df3c7173e7cfcf40e
AviHenri <avril.m.henry@gmail.com>
2020-07-01 09:16:43 +0100

    Tip 4 - auto nested unordered lists

---
+++ Courses/LinkedIn/PHP-TIPS&TRICKS/Tip_04/begin/loop.php
@@ -0,0 +1,10 @@
+<?php
+$skills = ['HTML', 'CSS', 'JavaScript', 'PHP', 'SQL'];
+
+$list = '<ul>';
+foreach ($skills as $skill) {
+    $list .= "<li>$skill</li>";
+}
+$list .= '</ul>';
+
+echo $list;
