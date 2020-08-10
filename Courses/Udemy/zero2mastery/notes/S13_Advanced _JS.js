f7583c90682da5cafa30c74794d4c9c6eb9c19e4
avihenri <avril.m.henry@gmail.com>
2020-08-10 11:29:25 +0100

    Pass by value vs Pass  by ref

--- Courses/Udemy/zero2mastery/notes/S13_Advanced _JS.js
+++ Courses/Udemy/zero2mastery/notes/S13_Advanced _JS.js
@@ -1,4 +1,26 @@
-// ADVANCED FUNCTIONS
+/* 
+* *** ADVANCED JAVASCRIPT ***
+   ~ = COMPLETE
+* SCOPE
+* CONTROL FLOW
+* ES5 & ES6
+* FUNCTIONS ~
+* ARRAYS ~
+* OBJECTS ~
+* - PASS BY VALUE VS PASS BY REFERENCE
+* - TYPE COERCION
+* ES7 (ES2016)
+* ES8 (ES2017)
+* ES9 (ES2018) - ES8 ASYNC AWAIT
+* ES10 (ES2019)
+* LOOPS
+* DEBUGGING
+* HOW JS WORKS
+*/
+
+/****************************/
+/* *** ADVANCED FUNCTIONS *** */
+/****************************/
 const first = () => {
   const greet = "Hi";
   const second = () => {
@@ -40,7 +62,9 @@ console.log("this is a side effect");
 // FUNCTIONAL PURITY = Deterministic - should always do the same thing
 // input -> into a function -> complete function -> return a value
 
-// ADVANCED ARRAYS
+/****************************/
+/* *** ADVANCED ARRAYS *** */
+/****************************/
 
 const array = [1, 2, 10, 16];
 
@@ -60,7 +84,6 @@ const mapArray = array.map((num) => {
 const newArr = array.map((num) => num * 2);
 
 // FILTER
-
 const filterArray = array.filter((num) => {
   return num > 5; // only add items that are greater than 5
 });
@@ -71,7 +94,9 @@ const reduceArray = array.reduce((accumulator, num) => {
   return accumulator + num;
 }, 5); // 34 - all array calculated together plus 5
 
-// ADVANCED OBJECTS
+/****************************/
+/* *** ADVANCED OBJECTS *** */
+/****************************/
 
 // REFERENCING TYPE
 var object1 = { value: 10 };
@@ -126,3 +151,55 @@ class Wizard extends Player {
 
 const wizard1 = new Wizard("Shelly", "Healer");
 const wizard2 = new Wizard("Shawn", "Dark Magic");
+
+/* *** PASS BY REF VS PASS BY VALUE *** */
+
+// pass by value
+let a = 5;
+let b = 10;
+let c = a; // copied value and put in new memory space
+
+c++;
+console.log(a); // 5
+console.log(c); // 6
+
+// pass by reference = objects
+let obj1 = { name: "Yao", password: "123" };
+let obj2 = obj1;
+
+obj2.password = "easy";
+
+console.log(obj1); // { name: 'Yao', password: 'easy' };
+console.log(obj2); // { name: 'Yao', password: 'easy' };
+// stored in memory and passed in reference so not copied like pass by value
+// both pointing to the same info
+// saves space in memory
+// arrays are the same as objects so passed by reference
+var c = [1, 2, 3, 4, 5];
+var d = c;
+d.push(5646546);
+console.log(c); // [1, 2, 3, 4, 5, 5646546]
+console.log(d); // [1, 2, 3, 4, 5, 5646546]
+
+var e = [].concat(c); // new pice of memory
+console.log(e); // [1, 2, 3, 4, 5]
+
+let obj = { a: "a", b: "b", c: "c" };
+let clone = Object.assign({}, obj);
+obj.c = 5;
+console.log(clone); // { a: 'a', b: 'b', c: 'c' }
+console.log(obj); // { a: 'a', b: 'b', c: 5 }
+let clone2 = { ...obj };
+console.log(clone2); // { a: 'a', b: 'b', c: 'c' }
+// only clones the first level = shallow clone
+// if we changed obj to be like the below (imagine obj2 s obj)
+let obj2 = { a: "a", b: "b", c: { deep: "clone me" } };
+obj2.c.deep = "you will change them all!";
+console.log(clone); // { a: 'a', b: 'b', c: { deep: 'clone me' }  }
+console.log(obj2); // { a: 'a', b: 'b', c: { deep: 'clone me' }  }
+console.log(clone2); // { a: 'a', b: 'b', c: { deep: 'clone me' }  }
+// the second level is another address in memory so you only clone the first layer
+// deep cloning -
+let superClone = JSON.parse(JSON.stringify(obj2)); // changes obj2 to a string then parses it back into an object
+console.log(superClone); // { a: "a", b: "b", c: { deep: "clone me" } }
+// WARNING: be careful - performance complications with big objects
