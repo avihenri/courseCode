0bb29f0608eb63f507a912646875a66bf0d45834
avihenri <avril.m.henry@gmail.com>
2020-08-21 08:32:17 +0100

    notes

---
+++ Courses/Udemy/zero2mastery/notes/S19-React-Redux.js
@@ -0,0 +1,15 @@
+// create-react-app - https://reactjs.org/docs/create-a-new-react-app.html
+// npm instal -g create-react-app
+//  create-react-app appName
+// npm start
+
+// UPDATING
+// package.json > update the version number
+// run npm install
+
+// create react app update
+// npx create-react-app my-app
+// cd my-app
+// npm start
+
+// npx -https://medium.com/@maybekatz/introducing-npx-an-npm-package-runner-55f7d4bd282b
